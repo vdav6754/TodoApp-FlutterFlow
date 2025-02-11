24c60920f315576034307875aeab9b8be2705505
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-02-11 01:34:13 +0000

    Updating to latest FlutterFlow output.

---
+++ lib/onboarding/onboarding_model.dart
@@ -0,0 +1,34 @@
+import '/flutter_flow/flutter_flow_util.dart';
+import 'onboarding_widget.dart' show OnboardingWidget;
+import 'package:flutter/material.dart';
+
+class OnboardingModel extends FlutterFlowModel<OnboardingWidget> {
+  ///  State fields for stateful widgets in this page.
+
+  bool isDataUploading = false;
+  FFUploadedFile uploadedLocalFile =
+      FFUploadedFile(bytes: Uint8List.fromList([]));
+  String uploadedFileUrl = '';
+
+  // State field(s) for TextField widget.
+  FocusNode? textFieldFocusNode1;
+  TextEditingController? textController1;
+  String? Function(BuildContext, String?)? textController1Validator;
+  DateTime? datePicked;
+  // State field(s) for TextField widget.
+  FocusNode? textFieldFocusNode2;
+  TextEditingController? textController2;
+  String? Function(BuildContext, String?)? textController2Validator;
+
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {
+    textFieldFocusNode1?.dispose();
+    textController1?.dispose();
+
+    textFieldFocusNode2?.dispose();
+    textController2?.dispose();
+  }
+}
